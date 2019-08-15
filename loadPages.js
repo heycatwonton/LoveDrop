197b2d706f0b3bfbc3fca6528e879b24ce654f56
catcattac <catwong.nyc@gmail.com>
2019-08-15 15:52:47 -0700

    added jquery to access nav pages inside content div

---
+++ loadPages.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $('#about').click(function () {
+        $('#content').load('about.html');
+    });
+});
+
+
+$(document).ready(function () {
+    $('#faq').click(function () {
+        $('#content').load('faq.html');
+    });
+});
+
+$(document).ready(function () {
+    $('#contact').click(function () {
+        $('#content').load('contact.html');
+    });
+});
+
